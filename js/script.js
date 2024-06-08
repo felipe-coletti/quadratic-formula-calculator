522f6272bcde7429006e237bb9668b6ef223faa2
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2024-06-08 14:56:55 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const findSolution = () => {
+    discriminant = b**2 - 4 * a * c
+
+    result = `A equação ${a}x² ${b >= 0 ? "+" : ""} ${b}x ${c >= 0 ? "+" : ""} ${c}`
+    
+    if (discriminant > 0) {
+        x1 = (-b + (discriminant**(1/2))) / (2 * a)
+        x2 = (-b - (discriminant**(1/2))) / (2 * a)
+
+        return result + ` tem duas soluções: x1 = ${x1} e x2 = ${x2}`
+    } else if (discriminant == 0) {
+        x = -b / (2 * a)
+
+        return result + `só tem uma solução: x = ${x}`
+    } else {
+        return result + " não apresenta raízes reais."
+    }
+}
+
+const getSolution = () => {
+    a = document.getElementById("a-input").value
+    b = document.getElementById("b-input").value
+    c = document.getElementById("c-input").value
+
+
+    if (a != '' && b != '' && c != ''){
+        document.getElementById("result").innerHTML = findSolution()
+        document.getElementById("result-area").style.display = "flex"
+    } else {
+        document.getElementById("result-area").style.display = "none"
+    }
+}
